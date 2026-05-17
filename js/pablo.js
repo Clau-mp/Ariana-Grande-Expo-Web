fcdc3d68fdc42780d0b7511a10f2a9366aca5fa4
P_SalvadorCamacho <pablosalvadorcamacho@gmail.com>
2026-05-17 19:58:23 +0200

    nav bar implemented

--- js/pablo.js
+++ js/pablo.js
@@ -75,3 +75,20 @@ if (ticketsForm) {
         event.preventDefault();
     });
 }
+
+
+
+// BARRA DE NAVEGACIÓN
+function openMenu() {
+
+    document.getElementById("menu").style.left = "0";
+    document.getElementById("bar-icon").style.display = "none";
+    document.getElementById("x-icon").style.display = "block";
+}
+
+
+function closeMenu() {
+    document.getElementById("menu").style.left = "-100%";
+     document.getElementById("bar-icon").style.display = "block";
+    document.getElementById("x-icon").style.display = "none";
+}
