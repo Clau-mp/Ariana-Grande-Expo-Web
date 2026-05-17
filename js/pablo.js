63e532fbc7273601c56d1918ed85bd227b858330
P_SalvadorCamacho <pablosalvadorcamacho@gmail.com>
2026-05-17 19:13:30 +0200

    html tickets finished

--- js/pablo.js
+++ js/pablo.js
@@ -38,3 +38,40 @@ if (gallery) {
 
 
 // GALLERY
+
+
+
+
+
+
+
+
+
+// TICKETS
+const ticketInputs = document.querySelectorAll(".tickets-quantity");
+const ticketTotal = document.getElementById("tickets-total-price");
+
+function calculateTicketsTotal() {
+    let total = 0;
+
+    ticketInputs.forEach(function (input) {
+        let quantity = Number(input.value);
+        let price = Number(input.getAttribute("data-price"));
+
+        total = total + quantity * price;
+    });
+
+    ticketTotal.innerHTML = total + "$";
+}
+
+ticketInputs.forEach(function (input) {
+    input.addEventListener("input", calculateTicketsTotal);
+});
+
+const ticketsForm = document.querySelector(".tickets-form");
+
+if (ticketsForm) {
+    ticketsForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+    });
+}
