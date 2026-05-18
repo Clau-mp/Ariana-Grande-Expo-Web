e42aff78efa03c6d505ac4f5a04a606c666c5760
Clau-mp <clau.mupan06@gmail.com>
2026-05-18 03:51:01 +0200

    carrusel finished

--- js/claudia.js
+++ js/claudia.js
@@ -15,19 +15,37 @@ function closeMenu() {
 }
 
 //  CARRUSEL
-var contendor = document.querySelector(".gallery-carrusel-container");
-var fotos = document.querySelectorAll(".carrusel-photo");
-var indiceActual = 1;
-function moverCarrusel(){
-    for(var i = 0; i < fotos.length; i++){
-        fotos[i].classList.remove("active");
+window.addEventListener("load", function() {
+    var contenedor = document.querySelector(".gallery-carrusel-container");
+    var fotos = document.querySelectorAll(".carrusel-photo");
+    var indiceActual = 1;
+    if(!contenedor || fotos.length === 0){return;}
+    function moverCarrusel(){
+
+        for(var i = 0; i < fotos.length; i++){
+            fotos[i].classList.remove("active");
+        }
+        if(!contenedor || fotos.length === 0){return}
+
+        if(fotos[indiceActual]){
+            fotos[indiceActual].classList.add("active");
+        }
+        var anchoFoto = fotos[0].offsetWidth;
+        var espacioGap = parseFloat(window.getComputedStyle(contenedor).gap) || 0;
+        var desplazamiento = (anchoFoto + espacioGap) * (indiceActual - 1);
+        contenedor.style.transform = "translateX(-" + desplazamiento + "px)";
     }
-    if (fotos[indiceActual]){
-        fotos[indiceActual].classList.add("active");
-    }
-    var anchoFoto = 350; 
-    if(fotos[0]){
-        anchoFoto = fotos[0].offsetWidth
-    }
-    var espacioGap = parseFloat(window.getComputedStyle(contendor).gap)
-}
+
+    moverCarrusel();
+    setInterval(function(){
+        indiceActual++;
+        if(indiceActual >= fotos.length - 1){
+            indiceActual = 1;
+        }
+        moverCarrusel();
+    },2500);
+
+});
+
+
+
