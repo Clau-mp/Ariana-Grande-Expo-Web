b390389ae5c557c01fd8556811667fae8faec3c5
Clau-mp <clau.mupan06@gmail.com>
2026-05-18 02:19:29 +0200

    carrusel caos

--- js/claudia.js
+++ js/claudia.js
@@ -1,3 +1,5 @@
+
+// BARRA DE NAVEGACIÓN
 function openMenu() {
 
     document.getElementById("menu").style.left = "0";
@@ -10,4 +12,22 @@ function closeMenu() {
     document.getElementById("menu").style.left = "-100%";
      document.getElementById("bar-icon").style.display = "block";
     document.getElementById("x-icon").style.display = "none";
+}
+
+//  CARRUSEL
+var contendor = document.querySelector(".gallery-carrusel-container");
+var fotos = document.querySelectorAll(".carrusel-photo");
+var indiceActual = 1;
+function moverCarrusel(){
+    for(var i = 0; i < fotos.length; i++){
+        fotos[i].classList.remove("active");
+    }
+    if (fotos[indiceActual]){
+        fotos[indiceActual].classList.add("active");
+    }
+    var anchoFoto = 350; 
+    if(fotos[0]){
+        anchoFoto = fotos[0].offsetWidth
+    }
+    var espacioGap = parseFloat(window.getComputedStyle(contendor).gap)
 }
