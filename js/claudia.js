cca79185143123abb0ab323051bf8cc7ca7629af
Clau-mp <clau.mupan06@gmail.com>
2026-05-18 17:21:04 +0200

    código js finished pantalla de carga

--- js/claudia.js
+++ js/claudia.js
@@ -1,4 +1,30 @@
 
+// PANTALLA DE CARGA
+
+let contador = 0;
+let floresAnimadas = false;
+function carga () {
+    if(contador  <= 100){
+        document.getElementById('porcentaje').innerHTML = contador + '%';
+        if(contador >= 90 && !floresAnimadas){
+            document.getElementById('loading-flowers-left').classList.add('flowers-bounce');
+            document.getElementById('loading-flowers-right').classList.add('flowers-bounce');
+            floresAnimadas = true
+        }
+        contador = contador + 1;
+        setTimeout(carga, 15);
+    } else{
+        setTimeout(() => {
+            document.getElementById('pantalla-de-carga').style.top = '-100%';
+        },1000);
+    }
+}
+
+window.onload = function(){
+    carga();
+};
+
+
 // BARRA DE NAVEGACIÓN
 function openMenu() {
 
